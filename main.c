43898401177288f39df1644a8d928df59183d190
inmisin <omeroflu2@gmail.com>
2026-08-05 17:34:17 +0300

    string parsing

--- main.c
+++ main.c
@@ -17,36 +17,59 @@ int main()
         {
             strSize++;
             char *temp = realloc(str, (strSize + 1) * sizeof(char));
-
             if (temp == NULL)
             {
                 write(1, "Memory is not allocated!\n", 25);
                 free(str);
                 return 0;
             }
+
             str = temp;
             str[strSize - 1] = token;
             str[strSize] = '\0'; // strcmp için
         }
         else
         {
+            if (str == NULL)
+            {
+                write(1, shellName, strlen(shellName));
+                continue;
+            }
 
-            if(strcmp(str, "exit") == 0)
+            if (strcmp(str, "exit") == 0)
             {
                 free(str);
                 break;
             }
+            else
+            {
+                char *piece = strtok(str, " ");
+                while (piece != NULL)
+                {
+                    /*
+                    write(1, piece, strlen(piece));
+                    write(1, "\n", 1);
+                    */
+                    
 
+                    piece = strtok(NULL, " ");
+                }
+            }
+
+            /*
             write(1, str, sizeof(char) * strSize);
             write(1, "\n", 1);
             free(str);
             str = NULL;
             strSize = 0;
+
+            */
+            free(str);
+            str = NULL;
+            strSize = 0;
             write(1, shellName, strlen(shellName));
         }
-        
     }
 
-    
     return 0;
 }
